34f8ae87b10cfcd6e0ff94381cc43848370effde
Himani Gangwar <himani.gangwar@w2c.co.in>
2024-05-14 18:33:08 +0530

    v1

--- server.ts
+++ server.ts
@@ -1,13 +1,11 @@
 
-// console.log("Hello server file")
-// import express from "express"
 const express = require("express")
 
 const app = express()
 
 require("dotenv").config()
-const port= process.env.PORT|| 4040
+const port= process.env.PORT|| 4040 
 
 app.listen(port,()=>{
-console.log(`Server is running at Port no. ${port}`)
+    console.log(`Server is running at Port no. ${port}`)
 })
